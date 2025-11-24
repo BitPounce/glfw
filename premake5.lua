a0ed7e7056ea7fc281e8a92a0ac28aea47a523ee
cats studio <79156677+iojuedioe9rd@users.noreply.github.com>
2025-11-24 20:24:13 +0000

    Add initial GLFW project configuration

---
+++ premake5.lua
@@ -0,0 +1,78 @@
+project "GLFW"
+    kind "StaticLib"     -- or SharedLib
+    language "C"
+    staticruntime "On"
+
+    targetdir ("bin/GLFW/%{cfg.buildcfg}")
+    objdir ("bin-int/GLFW/%{cfg.buildcfg}")
+
+    includedirs {
+        "include"
+    }
+
+    files {
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c",
+        "src/platform.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c"
+    }
+
+    filter "system:windows"
+        defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
+        files {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c"
+        }
+        links { "gdi32", "user32", "shell32" }
+
+    filter "system:macosx"
+        defines { "_GLFW_COCOA" }
+        files {
+            "src/cocoa_init.m",
+            "src/cocoa_monitor.m",
+            "src/cocoa_window.m",
+            "src/cocoa_joystick.m",
+            "src/nsgl_context.m",
+            "src/posix_thread.c",
+            "src/posix_time.c"
+        }
+        linkoptions { "-framework Cocoa", "-framework IOKit", "-framework CoreFoundation" }
+
+    filter "system:linux"
+        defines { "_GLFW_X11" }
+        files {
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_thread.c",
+            "src/posix_time.c",
+            "src/glx_context.c"
+        }
+        links { "X11", "pthread", "dl" }
+
+    -- Debug configuration
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    -- Release configuration
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+    -- Dist configuration
+    filter "configurations:Dist"
+        defines { "NDEBUG" }
+        optimize "Full"
+        symbols "Off"
