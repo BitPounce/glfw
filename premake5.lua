b9e57db6663f16bfff04eea1c0c37fc22dc9ff8b
cats studio <79156677+iojuedioe9rd@users.noreply.github.com>
2025-11-26 20:25:02 +0000

    fix

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 project "GLFW"
-    kind "StaticLib"     -- or SharedLib
+    kind "StaticLib"     -- or SharedLib if you need DLL
     language "C"
     staticruntime "On"
 
@@ -10,6 +10,9 @@ project "GLFW"
         "include"
     }
 
+    --
+    -- Common GLFW sources
+    --
     files {
         "src/context.c",
         "src/init.c",
@@ -19,11 +22,21 @@ project "GLFW"
         "src/window.c",
         "src/platform.c",
         "src/egl_context.c",
-        "src/osmesa_context.c"
+        "src/osmesa_context.c",
+
+        -- NULL platform backend (required even on Win32)
+        "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
 
+    --
+    -- WINDOWS (WIN32)
+    --
     filter "system:windows"
         defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
+
         files {
             "src/win32_init.c",
             "src/win32_joystick.c",
@@ -31,25 +44,18 @@ project "GLFW"
             "src/win32_time.c",
             "src/win32_thread.c",
             "src/win32_window.c",
-            "src/wgl_context.c"
+            "src/wgl_context.c",
+            "src/win32_module.c"   -- REQUIRED for module loading
         }
+
         links { "gdi32", "user32", "shell32" }
 
-    filter "system:macosx"
-        defines { "_GLFW_COCOA" }
-        files {
-            "src/cocoa_init.m",
-            "src/cocoa_monitor.m",
-            "src/cocoa_window.m",
-            "src/cocoa_joystick.m",
-            "src/nsgl_context.m",
-            "src/posix_thread.c",
-            "src/posix_time.c"
-        }
-        linkoptions { "-framework Cocoa", "-framework IOKit", "-framework CoreFoundation" }
-
+    --
+    -- LINUX (X11)
+    --
     filter "system:linux"
         defines { "_GLFW_X11" }
+
         files {
             "src/x11_init.c",
             "src/x11_monitor.c",
@@ -59,19 +65,48 @@ project "GLFW"
             "src/posix_time.c",
             "src/glx_context.c"
         }
+
         links { "X11", "pthread", "dl" }
 
-    -- Debug configuration
+    --
+    -- MACOS (COCOA)
+    --
+    filter "system:macosx"
+        defines { "_GLFW_COCOA" }
+
+        files {
+            "src/cocoa_init.m",
+            "src/cocoa_monitor.m",
+            "src/cocoa_window.m",
+            "src/cocoa_joystick.m",
+            "src/nsgl_context.m",
+            "src/posix_thread.c",
+            "src/posix_time.c"
+        }
+
+        linkoptions {
+            "-framework Cocoa",
+            "-framework IOKit",
+            "-framework CoreFoundation"
+        }
+
+    --
+    -- DEBUG
+    --
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
 
-    -- Release configuration
+    --
+    -- RELEASE
+    --
     filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
 
-    -- Dist configuration
+    --
+    -- DIST
+    --
     filter "configurations:Dist"
         defines { "NDEBUG" }
         optimize "Full"
